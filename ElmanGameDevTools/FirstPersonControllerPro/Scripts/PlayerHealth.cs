5dc296a155ba966ea133d060aef9f5c9aaf7ec44
jazzeze <118681551+jazzeze@users.noreply.github.com>
2026-03-03 13:04:38 +0000

    Add files via upload

---
+++ ElmanGameDevTools/FirstPersonControllerPro/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start() => currentHealth = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log("Player took damage. Current health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died");
+        //add death logic
+    }
+}
